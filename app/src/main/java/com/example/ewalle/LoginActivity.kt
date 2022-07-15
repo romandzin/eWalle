06f6b042f909c17fd15f984708b8a4573171bb49
nikita <sak-191_947433@volsu.ru>
2022-07-15 18:58:09 +0300

    Главный экран
    Сделана xml-разметка для главного экрана

--- app/src/main/java/com/example/ewalle/LoginActivity.kt
+++ app/src/main/java/com/example/ewalle/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.ewalle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         this.supportActionBar?.hide()
+    }
+
+    fun moveToMainScreen(view: View) {
+        val intent = Intent(this, MainScreenActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/ewalle/MainScreenActivity.kt
+++ app/src/main/java/com/example/ewalle/MainScreenActivity.kt
@@ -2,10 +2,12 @@ package com.example.ewalle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
     }
 }
